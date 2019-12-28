647bcb087177be4485020a56f7f6a6fbd682b20d
Tom Quisel <tom.quisel@gmail.com>
2019-12-27 19:25:35 -0800

    Add new counts and make them sortable

--- db/init/01-computed_aggregations.sql
+++ db/init/01-computed_aggregations.sql
@@ -7,6 +7,7 @@ RETURNS BOOLEAN as $$
     (trial.completion_date <= now() - INTERVAL '1 year');
 $$ language sql stable;
 
+-- is_late includes both late and missing (basically any noncompliant case)
 CREATE FUNCTION trials_status_schema.trials_is_late(trial trials_status_schema.trials)
 RETURNS BOOLEAN as $$
     SELECT
@@ -17,6 +18,34 @@ RETURNS BOOLEAN as $$
     );
 $$ language sql stable;
 
+-- only count trials as missing if results should have been reported by now
+CREATE FUNCTION trials_status_schema.trials_is_missing(trial trials_status_schema.trials)
+RETURNS BOOLEAN as $$
+    SELECT
+    trials_status_schema.trials_ready_for_report(trial) AND
+    trial.results_report_date IS NULL;
+$$ language sql stable;
+
+CREATE FUNCTION trials_status_schema.institutions_trial_count(institution trials_status_schema.institutions)
+RETURNS BIGINT as $$
+    SELECT COUNT(id)
+    FROM trials_status_schema.trials as trial_table
+    WHERE trial_table.org_id = institution.id;
+$$ language sql stable;
+COMMENT ON FUNCTION trials_status_schema.institutions_trial_count(institution trials_status_schema.institutions) IS
+E'@sortable';
+
+CREATE FUNCTION trials_status_schema.institutions_missing_results_count(institution trials_status_schema.institutions)
+RETURNS BIGINT as $$
+    SELECT COUNT(id)
+    FROM trials_status_schema.trials as trial_table
+    WHERE trial_table.org_id = institution.id AND
+    trials_status_schema.trials_is_missing(trial_table);
+$$ language sql stable;
+COMMENT ON FUNCTION trials_status_schema.institutions_missing_results_count(institution trials_status_schema.institutions) IS
+E'@sortable';
+
+
 CREATE FUNCTION trials_status_schema.institutions_late_report_count(institution trials_status_schema.institutions)
 RETURNS BIGINT as $$
     SELECT COUNT(trial_table.id)
@@ -24,6 +53,8 @@ RETURNS BIGINT as $$
     WHERE trial_table.org_id = institution.id AND
         trials_status_schema.trials_is_late(trial_table);
 $$ language sql stable;
+COMMENT ON FUNCTION trials_status_schema.institutions_late_report_count(institution trials_status_schema.institutions) IS
+E'@sortable';
 
 CREATE FUNCTION trials_status_schema.institutions_ready_for_report_count(institution trials_status_schema.institutions)
 RETURNS BIGINT as $$
@@ -32,6 +63,8 @@ RETURNS BIGINT as $$
     WHERE trial_table.org_id = institution.id AND
         trials_status_schema.trials_ready_for_report(trial_table);
 $$ language sql stable;
+COMMENT ON FUNCTION trials_status_schema.institutions_ready_for_report_count(institution trials_status_schema.institutions) IS
+E'@sortable';
 
 CREATE FUNCTION trials_status_schema.institutions_late_report_rate(institution trials_status_schema.institutions)
 RETURNS FLOAT as $$
